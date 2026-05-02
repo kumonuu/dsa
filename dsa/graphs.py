59ba023e9d58d1e1db5159f97420e516561c913f
kumonuu <kumonuu@proton.me>
2026-05-02 12:05:45 +0100

    Add files via upload

--- dsa/graphs.py
+++ dsa/graphs.py
@@ -5,10 +5,11 @@ graph = {
     "B": ["A","D"],
     "C": ["A","D","E"],
     "D": ["A","B","C","E"],
-    "E": ["C","D"]
+    "E": ["C","D"],
+    "F": []
 }
 
-def breadth_first(start):
+def bfs(start):
     visited = set([])
     queue = Queue()
 
@@ -23,9 +24,29 @@ def breadth_first(start):
                 queue.enqueue(item)
             visited.add(item)
 
+def has_path(graph,start,end):
+    if start == end:
+        return True
 
+    visited = set([])
+    queue = Queue()
+    queue.enqueue(start)
+    visited.add(start)
 
-breadth_first("A")
+    while queue.my_list:
+        key = queue.dequeue()
+        for item in graph[key]:
+            if item == end:
+                return True
+            if item not in visited:
+                visited.add(item)
+                queue.enqueue(item)
+    return False
+
+bfs("A")
+
+print(has_path(graph,"B","E"))
+print(has_path(graph,"A","F"))
         
 
         
